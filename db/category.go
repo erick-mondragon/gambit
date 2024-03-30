9be580b0f18c4026d93c574f0cf6bce525e3b697
unknown <erickmmondragon@gmail.com>
2024-03-30 00:07:15 -0500

    Add Update Category

--- db/category.go
+++ db/category.go
@@ -3,8 +3,11 @@ package db
 import (
 	"database/sql"
 	"fmt"
+	"strconv"
+	"strings"
 
 	"github.com/erick-mondragon/gambit/models"
+	"github.com/erick-mondragon/gambit/tools"
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -33,4 +36,37 @@ func InsertCategory(c models.Category) (int64, error) {
 
 	fmt.Println("Insert Category > Ejecución Éxitosa")
 	return LastInsertId, err2
+}
+
+func UpdateCategory(c models.Category) error {
+	fmt.Println("Comienza registro UpdateCategory")
+
+	err := DbConnect()
+	if err != nil {
+		return err
+	}
+	defer Db.Close()
+
+	sentencia := "UPDATE category SET "
+	if len(c.CategName) > 0 {
+		sentencia += "Categ_Name = '" + tools.EscapeString(c.CategName) + "'"
+	}
+
+	if len(c.CategPath) > 0 {
+		if !strings.HasSuffix(sentencia, "SET ") {
+			sentencia += ", "
+		}
+		sentencia += "Categ_Path = '" + tools.EscapeString(c.CategPath) + "'"
+	}
+
+	sentencia += " WHERE Categ_Id = " + strconv.Itoa(c.CategID)
+
+	_, err = Db.Exec(sentencia)
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+
+	fmt.Println("Update Category > Ejecución éxitosa")
+	return nil
 }
--- routers/category.go
+++ routers/category.go
@@ -35,3 +35,30 @@ func InsertCategory(body string, User string) (int, string) {
 
 	return 200, "{ CategID: " + strconv.Itoa(int(result)) + "}"
 }
+
+func UpdateCategory(body string, User string, id int) (int, string) {
+	var t models.Category
+
+	err := json.Unmarshal([]byte(body), &t)
+	if err != nil {
+		return 400, "Error en los datos recibidos " + err.Error()
+	}
+
+	if len(t.CategName) == 0 && len(t.CategPath) == 0 {
+		return 400, "Debe especificar el Nombre y el Path de la categoría para actualizar"
+	}
+
+	isAdmin, msg := db.UserIsAdmin(User)
+	if !isAdmin {
+		return 400, msg
+	}
+
+	t.CategID = id
+	err2 := db.UpdateCategory(t)
+	if err2 != nil {
+		return 400, "Ocurrió un error al intentar realizar el UPDATE de la categoría " + strconv.Itoa(id) + " > " + err2.Error()
+	}
+
+	return 200, "Update OK"
+
+}
