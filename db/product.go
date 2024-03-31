c18cfcce91742cfa5def58db9c1c7b7e2dcc43b3
unknown <erickmmondragon@gmail.com>
2024-03-31 02:09:09 -0500

    Fix Select Product

--- db/product.go
+++ db/product.go
@@ -141,7 +141,7 @@ func SelectProduct(p models.Product, choice string, page int, pageSize int, orde
 	var sentenciaCount string
 	var where, limit string
 
-	sentencia = "SELECT Prod_Id, Prod_Title, Prod_Description, Prod_CreateAt, Prod_Updated, Prod_Price, Prod_Path, Prod_CategoryId, Prod_Stock FROM products"
+	sentencia = "SELECT Prod_Id, Prod_Title, Prod_Description, Prod_CreatedAt, Prod_Updated, Prod_Price, Prod_Path, Prod_CategoryId, Prod_Stock FROM products"
 	sentenciaCount = "SELECT COUNT(*) AS registros FROM products "
 
 	switch choice {
